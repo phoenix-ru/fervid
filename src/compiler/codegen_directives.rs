6b67aa23242ca2dba7b0fa432cb29b698dfbc509
Marsel Shayhin <phoenix.apps@yahoo.com>
2023-04-09 23:36:58 +0200

    style: improve readability in codegen directives

--- src/compiler/codegen_directives.rs
+++ src/compiler/codegen_directives.rs
@@ -9,88 +9,94 @@ impl<'a> CodegenContext<'a> {
     CodeHelper::open_sq_bracket(buf);
 
     for attr in &starting_tag.attributes {
-      if let HtmlAttribute::VDirective (VDirective { name, argument, modifiers, value, is_dynamic_slot }) = attr {
-        if !supports_with_directive(*name, is_component) {
-          continue;
-        }
+      let HtmlAttribute::VDirective (VDirective {
+        name,
+        argument,
+        modifiers,
+        value,
+        is_dynamic_slot: _
+      }) = attr else { continue };
 
+      if !supports_with_directive(*name, is_component) {
+        continue;
+      }
+
+      self.code_helper.indent();
+      self.code_helper.newline(buf);
+
+      // todo generate
+
+      // Whether to split generated input across multiple lines or inline in one
+      let has_argument = argument.len() > 0;
+      let has_modifiers = modifiers.len() > 0;
+      let has_arg_or_modifiers = has_argument || has_modifiers;
+
+      // A directive is an array of
+      // [<directive_ident>, <directive_value>?, <directive_arg>?, <directive_modifiers>?]
+      CodeHelper::open_sq_bracket(buf);
+      if has_arg_or_modifiers {
         self.code_helper.indent();
         self.code_helper.newline(buf);
+      }
 
-        // todo generate
+      // Write <directive_ident>. This is either from Vue (vModel*) or the identifier of custom directive
+      // TODO better handle `is_component`
+      if *name == "model" && !is_component {
+        let vmodel_directive = self.get_vmodel_directive_name(starting_tag);
+        buf.push_str(vmodel_directive);
+      } else if *name == "show" {
+        // v-show comes from "vue" import
+        buf.push_str(self.get_and_add_import_str(VueImports::VShow));
+      } else {
+        self.add_to_directives_and_write(buf, name);
+      }
 
-        // Whether to split generated input across multiple lines or inline in one
-        let has_argument = argument.len() > 0;
-        let has_modifiers = modifiers.len() > 0;
-        let has_arg_or_modifiers = has_argument || has_modifiers;
-
-        // A directive is an array of
-        // [<directive_ident>, <directive_value>?, <directive_arg>?, <directive_modifiers>?]
-        CodeHelper::open_sq_bracket(buf);
+      // <directive_value>?
+      if let Some(directive_value) = *value {
         if has_arg_or_modifiers {
-          self.code_helper.indent();
-          self.code_helper.newline(buf);
-        }
-
-        // Write <directive_ident>. This is either from Vue (vModel*) or the identifier of custom directive
-        // TODO better handle `is_component`
-        if *name == "model" && !is_component {
-          let vmodel_directive = self.get_vmodel_directive_name(starting_tag);
-          buf.push_str(vmodel_directive);
-        } else if *name == "show" {
-          // v-show comes from "vue" import
-          buf.push_str(self.get_and_add_import_str(VueImports::VShow));
-        } else {
-          self.add_to_directives_and_write(buf, name);
-        }
-
-        // <directive_value>?
-        if let Some(directive_value) = *value {
-          if has_arg_or_modifiers {
-            self.code_helper.comma_newline(buf);
-          } else {
-            CodeHelper::comma(buf);
-          }
-
-          // TODO use context-dependent variables (not msg, but _ctx.msg or $setup.msg)
-          buf.push_str("_ctx.");
-          buf.push_str(directive_value)
-        } else if has_arg_or_modifiers {
           self.code_helper.comma_newline(buf);
+        } else {
+          CodeHelper::comma(buf);
+        }
+
+        // TODO use context-dependent variables (not msg, but _ctx.msg or $setup.msg)
+        buf.push_str("_ctx.");
+        buf.push_str(directive_value)
+      } else if has_arg_or_modifiers {
+        self.code_helper.comma_newline(buf);
+        buf.push_str("void 0")
+      }
+
+      // <directive_arg>?
+      if has_arg_or_modifiers {
+        self.code_helper.comma_newline(buf);
+
+        if has_argument {
+          CodeHelper::quoted(buf, argument)
+        } else {
           buf.push_str("void 0")
         }
-
-        // <directive_arg>?
-        if has_arg_or_modifiers {
-          self.code_helper.comma_newline(buf);
-
-          if has_argument {
-            CodeHelper::quoted(buf, argument)
-          } else {
-            buf.push_str("void 0")
-          }
-        }
-
-        // <directive_modifiers>?
-        if has_modifiers {
-          self.code_helper.comma_newline(buf);
-
-          // Generates a Js object, where keys are modifier names and values are `true`
-          // For example, `v-directive:prop.foo.bar` would have `{ foo: true, bar: true }`
-          self.code_helper.obj_from_entries_iter(
-            buf,
-            modifiers.iter().map(|modifier| (*modifier, "true"))
-          );
-        }
-
-        if has_arg_or_modifiers {
-          self.code_helper.unindent();
-          self.code_helper.newline(buf);
-        }
-
-        CodeHelper::close_sq_bracket(buf);
-        self.code_helper.unindent();
       }
+
+      // <directive_modifiers>?
+      if has_modifiers {
+        self.code_helper.comma_newline(buf);
+
+        // Generates a Js object, where keys are modifier names and values are `true`
+        // For example, `v-directive:prop.foo.bar` would have `{ foo: true, bar: true }`
+        self.code_helper.obj_from_entries_iter(
+          buf,
+          modifiers.iter().map(|modifier| (*modifier, "true"))
+        );
+      }
+
+      if has_arg_or_modifiers {
+        self.code_helper.unindent();
+        self.code_helper.newline(buf);
+      }
+
+      CodeHelper::close_sq_bracket(buf);
+      self.code_helper.unindent();
     }
 
     self.code_helper.newline(buf);
