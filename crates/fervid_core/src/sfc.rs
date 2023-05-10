2fca5c49e9691eee39dac72acb2897e3edae0ddf
Marsel Shayhin <phoenix.apps@yahoo.com>
2023-05-10 21:57:54 +0200

    enh: add Debug trait to SFC blocks

--- crates/fervid_core/src/sfc.rs
+++ crates/fervid_core/src/sfc.rs
@@ -8,33 +8,33 @@ pub struct SfcDescriptor<'a> {
   pub custom_blocks: Vec<SfcCustomBlock<'a>>
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct SfcTemplateBlock<'a> {
   pub lang: &'a str,
   pub roots: Vec<Node<'a>>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct SfcScriptBlock<'a> {
   pub lang: &'a str,
   pub content: &'a str,
   pub is_setup: bool,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct SfcStyleBlock<'a> {
   pub lang: &'a str,
   pub content: &'a str,
   pub is_scoped: bool,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct SfcCustomBlock<'a> {
   pub starting_tag: StartingTag<'a>,
   pub content: &'a str
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum SfcBlock<'a> {
   Template(SfcTemplateBlock<'a>),
   Script(SfcScriptBlock<'a>),
