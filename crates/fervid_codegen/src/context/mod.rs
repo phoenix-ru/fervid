14f0f4120b5f37876315e436e5d441e0480529d1
Marsel Shayhin <phoenix.apps@yahoo.com>
2023-06-30 15:08:54 +0200

    enh: use `flagset` crate for import management

--- crates/fervid_codegen/src/context/mod.rs
+++ crates/fervid_codegen/src/context/mod.rs
@@ -1,11 +1,12 @@
+use flagset::FlagSet;
 use fxhash::FxHashMap as HashMap;
 use swc_core::ecma::atoms::JsWord;
 
-use crate::transform::MockScopeHelper;
+use crate::{transform::MockScopeHelper, imports::VueImports};
 
 #[derive(Debug, Default)]
 pub struct CodegenContext {
     pub components: HashMap<String, JsWord>,
-    pub used_imports: u64,
+    pub used_imports: FlagSet<VueImports>,
     pub scope_helper: MockScopeHelper
 }
--- crates/fervid_codegen/src/imports/mod.rs
+++ crates/fervid_codegen/src/imports/mod.rs
@@ -1,141 +1,88 @@
-use swc_core::ecma::atoms::JsWord;
+use flagset::flags;
+use swc_core::{
+    common::DUMMY_SP,
+    ecma::{
+        ast::{
+            Ident, ImportNamedSpecifier, ImportSpecifier,
+            ModuleExportName,
+        },
+        atoms::JsWord,
+    },
+};
 
 use super::context::CodegenContext;
 
-// TODO refactor to a macro
-// This is annoying me a lot
-
-static CREATE_BLOCK: &str = "_createBlock";
-static CREATE_COMMENT_VNODE: &str = "_createCommentVNode";
-static CREATE_ELEMENT_BLOCK: &str = "_createElementBlock";
-static CREATE_ELEMENT_VNODE: &str = "_createElementVNode";
-static CREATE_TEXT_VNODE: &str = "_createTextVNode";
-static CREATE_VNODE: &str = "_createVNode";
-static FRAGMENT: &str = "_Fragment";
-static KEEP_ALIVE: &str = "_KeepAlive";
-static NORMALIZE_CLASS: &str = "_normalizeClass";
-static NORMALIZE_STYLE: &str = "_normalizeStyle";
-static OPEN_BLOCK: &str = "_openBlock";
-static RENDER_LIST: &str = "_renderList";
-static RENDER_SLOT: &str = "_renderSlot";
-static RESOLVE_COMPONENT: &str = "_resolveComponent";
-static RESOLVE_DIRECTIVE: &str = "_resolveDirective";
-static RESOLVE_DYNAMIC_COMPONENT: &str = "_resolveDynamicComponent";
-static SUSPENSE: &str = "_Suspense";
-static TELEPORT: &str = "_Teleport";
-static TO_DISPLAY_STRING: &str = "_toDisplayString";
-static TRANSITION: &str = "_Transition";
-static TRANSITION_GROUP: &str = "_TransitionGroup";
-static VMODEL_CHECKBOX: &str = "_vModelCheckbox";
-static VMODEL_RADIO: &str = "_vModelRadio";
-static VMODEL_SELECT: &str = "_vModelSelect";
-static VMODEL_TEXT: &str = "_vModelText";
-static VSHOW: &str = "_vShow";
-static WITH_CTX: &str = "_withCtx";
-static WITH_DIRECTIVES: &str = "_withDirectives";
-static WITH_MODIFIERS: &str = "_withModifiers";
-
-#[derive(Clone, Copy)]
-pub enum VueImports {
-    CreateBlock,
-    CreateCommentVNode,
-    CreateElementBlock,
-    CreateElementVNode,
-    CreateTextVNode,
-    CreateVNode,
-    Fragment,
-    KeepAlive,
-    NormalizeClass,
-    NormalizeStyle,
-    OpenBlock,
-    RenderList,
-    RenderSlot,
-    ResolveComponent,
-    ResolveDirective,
-    ResolveDynamicComponent,
-    Suspense,
-    Teleport,
-    ToDisplayString,
-    Transition,
-    TransitionGroup,
-    VModelCheckbox,
-    VModelRadio,
-    VModelSelect,
-    VModelText,
-    VShow,
-    WithCtx,
-    WithDirectives,
-    WithModifiers,
+flags! {
+    // #[derive(Clone, Copy)]
+    pub enum VueImports: u64 {
+        CreateBlock,
+        CreateCommentVNode,
+        CreateElementBlock,
+        CreateElementVNode,
+        CreateTextVNode,
+        CreateVNode,
+        Fragment,
+        KeepAlive,
+        NormalizeClass,
+        NormalizeStyle,
+        OpenBlock,
+        RenderList,
+        RenderSlot,
+        ResolveComponent,
+        ResolveDirective,
+        ResolveDynamicComponent,
+        Suspense,
+        Teleport,
+        ToDisplayString,
+        Transition,
+        TransitionGroup,
+        VModelCheckbox,
+        VModelRadio,
+        VModelSelect,
+        VModelText,
+        VShow,
+        WithCtx,
+        WithDirectives,
+        WithModifiers,
+    }
 }
-
-static ALL_IMPORTS: [VueImports; 29] = [
-    VueImports::CreateBlock,
-    VueImports::CreateCommentVNode,
-    VueImports::CreateElementBlock,
-    VueImports::CreateElementVNode,
-    VueImports::CreateTextVNode,
-    VueImports::CreateVNode,
-    VueImports::Fragment,
-    VueImports::KeepAlive,
-    VueImports::NormalizeClass,
-    VueImports::NormalizeStyle,
-    VueImports::OpenBlock,
-    VueImports::RenderList,
-    VueImports::RenderSlot,
-    VueImports::ResolveComponent,
-    VueImports::ResolveDirective,
-    VueImports::ResolveDynamicComponent,
-    VueImports::Suspense,
-    VueImports::Teleport,
-    VueImports::ToDisplayString,
-    VueImports::Transition,
-    VueImports::TransitionGroup,
-    VueImports::VModelCheckbox,
-    VueImports::VModelRadio,
-    VueImports::VModelSelect,
-    VueImports::VModelText,
-    VueImports::VShow,
-    VueImports::WithCtx,
-    VueImports::WithDirectives,
-    VueImports::WithModifiers,
-];
 
 impl CodegenContext {
     pub fn add_to_imports(&mut self, vue_import: VueImports) {
-        self.used_imports |= Self::get_import_mask_bit(vue_import);
+        self.used_imports |= vue_import;
     }
 
     pub fn get_import_str(vue_import: VueImports) -> &'static str {
         match vue_import {
-            VueImports::CreateBlock => CREATE_BLOCK,
-            VueImports::CreateCommentVNode => CREATE_COMMENT_VNODE,
-            VueImports::CreateElementBlock => CREATE_ELEMENT_BLOCK,
-            VueImports::CreateElementVNode => CREATE_ELEMENT_VNODE,
-            VueImports::CreateTextVNode => CREATE_TEXT_VNODE,
-            VueImports::CreateVNode => CREATE_VNODE,
-            VueImports::Fragment => FRAGMENT,
-            VueImports::KeepAlive => KEEP_ALIVE,
-            VueImports::NormalizeClass => NORMALIZE_CLASS,
-            VueImports::NormalizeStyle => NORMALIZE_STYLE,
-            VueImports::OpenBlock => OPEN_BLOCK,
-            VueImports::RenderList => RENDER_LIST,
-            VueImports::RenderSlot => RENDER_SLOT,
-            VueImports::ResolveComponent => RESOLVE_COMPONENT,
-            VueImports::ResolveDirective => RESOLVE_DIRECTIVE,
-            VueImports::ResolveDynamicComponent => RESOLVE_DYNAMIC_COMPONENT,
-            VueImports::Suspense => SUSPENSE,
-            VueImports::Teleport => TELEPORT,
-            VueImports::ToDisplayString => TO_DISPLAY_STRING,
-            VueImports::Transition => TRANSITION,
-            VueImports::TransitionGroup => TRANSITION_GROUP,
-            VueImports::VModelCheckbox => VMODEL_CHECKBOX,
-            VueImports::VModelRadio => VMODEL_RADIO,
-            VueImports::VModelSelect => VMODEL_SELECT,
-            VueImports::VModelText => VMODEL_TEXT,
-            VueImports::VShow => VSHOW,
-            VueImports::WithCtx => WITH_CTX,
-            VueImports::WithDirectives => WITH_DIRECTIVES,
-            VueImports::WithModifiers => WITH_MODIFIERS,
+            VueImports::CreateBlock => "_createBlock",
+            VueImports::CreateCommentVNode => "_createCommentVNode",
+            VueImports::CreateElementBlock => "_createElementBlock",
+            VueImports::CreateElementVNode => "_createElementVNode",
+            VueImports::CreateTextVNode => "_createTextVNode",
+            VueImports::CreateVNode => "_createVNode",
+            VueImports::Fragment => "_Fragment",
+            VueImports::KeepAlive => "_KeepAlive",
+            VueImports::NormalizeClass => "_normalizeClass",
+            VueImports::NormalizeStyle => "_normalizeStyle",
+            VueImports::OpenBlock => "_openBlock",
+            VueImports::RenderList => "_renderList",
+            VueImports::RenderSlot => "_renderSlot",
+            VueImports::ResolveComponent => "_resolveComponent",
+            VueImports::ResolveDirective => "_resolveDirective",
+            VueImports::ResolveDynamicComponent => "_resolveDynamicComponent",
+            VueImports::Suspense => "_Suspense",
+            VueImports::Teleport => "_Teleport",
+            VueImports::ToDisplayString => "_toDisplayString",
+            VueImports::Transition => "_Transition",
+            VueImports::TransitionGroup => "_TransitionGroup",
+            VueImports::VModelCheckbox => "_vModelCheckbox",
+            VueImports::VModelRadio => "_vModelRadio",
+            VueImports::VModelSelect => "_vModelSelect",
+            VueImports::VModelText => "_vModelText",
+            VueImports::VShow => "_vShow",
+            VueImports::WithCtx => "_withCtx",
+            VueImports::WithDirectives => "_withDirectives",
+            VueImports::WithModifiers => "_withModifiers",
         }
     }
 
@@ -154,20 +101,15 @@ impl CodegenContext {
     }
 
     pub fn generate_imports_string(&self) -> String {
-        let imports_mask = self.used_imports;
         let mut result = String::from("import { ");
         let mut has_first_import = false;
 
-        for import in ALL_IMPORTS.iter() {
-            if Self::get_import_mask_bit(*import) & imports_mask == 0 {
-                continue;
-            }
-
+        for import in self.used_imports.into_iter() {
             if has_first_import {
                 result.push_str(", ");
             }
 
-            let import_str = Self::get_import_str(*import);
+            let import_str = Self::get_import_str(import);
             result.push_str(&import_str[1..]);
             result.push_str(" as ");
             result.push_str(import_str);
@@ -180,37 +122,86 @@ impl CodegenContext {
         result
     }
 
-    fn get_import_mask_bit(vue_import: VueImports) -> u64 {
-        match vue_import {
-            VueImports::CreateBlock => 1 << 0,
-            VueImports::CreateCommentVNode => 1 << 1,
-            VueImports::CreateElementBlock => 1 << 2,
-            VueImports::CreateElementVNode => 1 << 3,
-            VueImports::CreateTextVNode => 1 << 4,
-            VueImports::CreateVNode => 1 << 5,
-            VueImports::Fragment => 1 << 6,
-            VueImports::KeepAlive => 1 << 7,
-            VueImports::NormalizeClass => 1 << 25,
-            VueImports::NormalizeStyle => 1 << 26,
-            VueImports::OpenBlock => 1 << 34,
-            VueImports::RenderList => 1 << 35,
-            VueImports::RenderSlot => 1 << 36,
-            VueImports::ResolveComponent => 1 << 37,
-            VueImports::ResolveDirective => 1 << 38,
-            VueImports::ResolveDynamicComponent => 1 << 39,
-            VueImports::Suspense => 1 << 40,
-            VueImports::Teleport => 1 << 41,
-            VueImports::ToDisplayString => 1 << 42,
-            VueImports::Transition => 1 << 43,
-            VueImports::TransitionGroup => 1 << 44,
-            VueImports::VModelCheckbox => 1 << 45,
-            VueImports::VModelRadio => 1 << 46,
-            VueImports::VModelSelect => 1 << 47,
-            VueImports::VModelText => 1 << 48,
-            VueImports::VShow => 1 << 49,
-            VueImports::WithCtx => 1 << 55,
-            VueImports::WithDirectives => 1 << 56,
-            VueImports::WithModifiers => 1 << 57,
+    /// Generates all the imports used by template generation.
+    /// All of the imports come from 'vue'.
+    pub fn generate_imports(&self) -> Vec<ImportSpecifier> {
+        let mut result = Vec::new();
+        for import in self.used_imports.into_iter() {
+            let import_raw = Self::get_import_str(import);
+
+            let import_local = Ident {
+                span: DUMMY_SP,
+                sym: JsWord::from(import_raw),
+                optional: false,
+            };
+
+            let import_vue = Some(ModuleExportName::Ident(Ident {
+                span: DUMMY_SP,
+                sym: JsWord::from(&import_raw[1..]),
+                optional: false,
+            }));
+
+            result.push(ImportSpecifier::Named(ImportNamedSpecifier {
+                span: DUMMY_SP,
+                local: import_local,
+                imported: import_vue,
+                is_type_only: false,
+            }));
         }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use swc_core::ecma::ast::{Str, ImportDecl};
+
+    #[test]
+    fn it_remembers_added_imports() {
+        let mut ctx = CodegenContext::default();
+
+        // Semi-random imports (one is duplicated)
+        ctx.add_to_imports(VueImports::OpenBlock);
+        ctx.add_to_imports(VueImports::CreateBlock);
+        ctx.add_to_imports(VueImports::WithCtx);
+        ctx.add_to_imports(VueImports::NormalizeClass);
+        ctx.add_to_imports(VueImports::ToDisplayString);
+        ctx.add_to_imports(VueImports::WithDirectives);
+        ctx.add_to_imports(VueImports::WithModifiers);
+        ctx.add_to_imports(VueImports::OpenBlock);
+
+        assert_eq!(7, ctx.used_imports.into_iter().count());
+    }
+
+    #[test]
+    fn it_generates_imports() {
+        let mut ctx = CodegenContext::default();
+
+        // Semi-random imports (one is duplicated)
+        ctx.add_to_imports(VueImports::OpenBlock);
+        ctx.add_to_imports(VueImports::CreateBlock);
+        ctx.add_to_imports(VueImports::WithCtx);
+        ctx.add_to_imports(VueImports::NormalizeClass);
+        ctx.add_to_imports(VueImports::ToDisplayString);
+        ctx.add_to_imports(VueImports::WithDirectives);
+        ctx.add_to_imports(VueImports::WithModifiers);
+        ctx.add_to_imports(VueImports::OpenBlock);
+
+        let generated_imports = ctx.generate_imports();
+        let vue_import_decl = ImportDecl {
+            span: DUMMY_SP,
+            specifiers: generated_imports,
+            src: Box::new(Str {
+                span: DUMMY_SP,
+                value: "vue".into(),
+                raw: None,
+            }),
+            type_only: false,
+            asserts: None,
+        };
+
+        assert_eq!(crate::test_utils::to_str(vue_import_decl), "import{createBlock as _createBlock,normalizeClass as _normalizeClass,openBlock as _openBlock,toDisplayString as _toDisplayString,withCtx as _withCtx,withDirectives as _withDirectives,withModifiers as _withModifiers}from\"vue\";");
     }
 }
