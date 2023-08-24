5f4e525b2e56ae4c0e6d376cbb8c74ec70332602
Marsel Shaikhin <phoenix.apps@yahoo.com>
2023-08-24 17:58:35 +0200

    feat: implement KeepAlive builtin component

---
+++ crates/fervid_codegen/src/builtins/keepalive.rs
@@ -0,0 +1,175 @@
+use fervid_core::ElementNode;
+use swc_core::{
+    common::DUMMY_SP,
+    ecma::ast::{ArrayLit, Expr, ExprOrSpread, Ident, ObjectLit},
+};
+
+use crate::{imports::VueImports, CodegenContext};
+
+impl CodegenContext {
+    /// Generates `(_openBlock(), _createBlock(_KeepAlive, null, [keepalive_children], 1024))`
+    pub fn generate_keepalive(&mut self, element_node: &ElementNode) -> Expr {
+        let span = DUMMY_SP; // TODO
+
+        // _KeepAlive
+        let keepalive_identifier = Expr::Ident(Ident {
+            span,
+            sym: self.get_and_add_import_ident(VueImports::KeepAlive),
+            optional: false,
+        });
+
+        let keepalive_attrs = if element_node.starting_tag.attributes.len() != 0 {
+            let mut attrs = Vec::with_capacity(element_node.starting_tag.attributes.len());
+            self.generate_attributes(&element_node.starting_tag.attributes, &mut attrs);
+            Some(Expr::Object(ObjectLit { span, props: attrs }))
+        } else {
+            None
+        };
+
+        let generated_children = self.generate_element_children(element_node, false);
+        let keepalive_children = if generated_children.0.len() != 0 {
+            Some(Expr::Array(ArrayLit {
+                span,
+                elems: generated_children
+                    .0
+                    .into_iter()
+                    .map(|c| {
+                        Some(ExprOrSpread {
+                            spread: None,
+                            expr: Box::new(c),
+                        })
+                    })
+                    .collect(),
+            }))
+        } else {
+            None
+        };
+
+        let should_wrap_in_block = keepalive_children.is_some();
+        let patch_flag = if should_wrap_in_block { 1024 } else { 0 };
+
+        self.generate_componentlike(
+            keepalive_identifier,
+            keepalive_attrs,
+            keepalive_children,
+            patch_flag,
+            should_wrap_in_block,
+            span,
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fervid_core::{AttributeOrBinding, BuiltinType, ElementKind, StartingTag, Node};
+
+    use crate::test_utils::js;
+
+    use super::*;
+
+    #[test]
+    fn it_generates_empty_keepalive() {
+        // <keep-alive></keep-alive>
+        test_out(
+            ElementNode {
+                kind: ElementKind::Builtin(BuiltinType::KeepAlive),
+                starting_tag: StartingTag {
+                    tag_name: "keep-alive",
+                    attributes: vec![],
+                    directives: None,
+                },
+                children: vec![],
+                template_scope: 0,
+            },
+            r#"_createVNode(_KeepAlive)"#,
+        )
+    }
+
+    #[test]
+    fn it_generates_keepalive_attrs() {
+        // <keep-alive foo="bar" :baz="qux"></keep-alive>
+        test_out(
+            ElementNode {
+                kind: ElementKind::Builtin(BuiltinType::KeepAlive),
+                starting_tag: StartingTag {
+                    tag_name: "keep-alive",
+                    attributes: vec![
+                        AttributeOrBinding::RegularAttribute {
+                            name: "foo",
+                            value: "bar",
+                        },
+                        AttributeOrBinding::VBind(fervid_core::VBindDirective {
+                            argument: Some("baz".into()),
+                            value: js("qux"),
+                            is_camel: false,
+                            is_prop: false,
+                            is_attr: false,
+                        }),
+                    ],
+                    directives: None,
+                },
+                children: vec![],
+                template_scope: 0,
+            },
+            r#"_createVNode(_KeepAlive,{foo:"bar",baz:qux})"#,
+        )
+    }
+
+    #[test]
+    fn it_generates_keepalive_children() {
+        // <keep-alive>foobar</keep-alive>
+        test_out(
+            ElementNode {
+                kind: ElementKind::Builtin(BuiltinType::KeepAlive),
+                starting_tag: StartingTag {
+                    tag_name: "keep-alive",
+                    attributes: vec![],
+                    directives: None,
+                },
+                children: vec![
+                    Node::Text("foobar")
+                ],
+                template_scope: 0,
+            },
+            r#"(_openBlock(),_createBlock(_KeepAlive,null,[_createTextVNode("foobar")],1024))"#,
+        )
+    }
+
+    #[test]
+    fn it_generates_full_keepalive() {
+        // <keep-alive foo="bar" :baz="qux">foobar</keep-alive>
+        test_out(
+            ElementNode {
+                kind: ElementKind::Builtin(BuiltinType::KeepAlive),
+                starting_tag: StartingTag {
+                    tag_name: "keep-alive",
+                    attributes: vec![
+                        AttributeOrBinding::RegularAttribute {
+                            name: "foo",
+                            value: "bar",
+                        },
+                        AttributeOrBinding::VBind(fervid_core::VBindDirective {
+                            argument: Some("baz".into()),
+                            value: js("qux"),
+                            is_camel: false,
+                            is_prop: false,
+                            is_attr: false,
+                        }),
+                    ],
+                    directives: None,
+                },
+                children: vec![
+                    Node::Text("foobar")
+                ],
+                template_scope: 0,
+            },
+            r#"(_openBlock(),_createBlock(_KeepAlive,{foo:"bar",baz:qux},[_createTextVNode("foobar")],1024))"#,
+        )
+    }
+
+    fn test_out(input: ElementNode, expected: &str) {
+        let mut ctx = CodegenContext::default();
+        let out = ctx.generate_keepalive(&input);
+        assert_eq!(crate::test_utils::to_str(out), expected)
+    }
+}
--- crates/fervid_codegen/src/builtins/mod.rs
+++ crates/fervid_codegen/src/builtins/mod.rs
@@ -3,12 +3,13 @@ use swc_core::ecma::ast::Expr;
 
 use crate::CodegenContext;
 
+mod keepalive;
 mod slot;
 
 impl CodegenContext {
     pub fn generate_builtin(&mut self, element_node: &ElementNode, builtin_type: BuiltinType) -> Expr {
         match builtin_type {
-            BuiltinType::KeepAlive => todo!(),
+            BuiltinType::KeepAlive => self.generate_keepalive(element_node),
             BuiltinType::Slot => self.generate_slot(element_node),
             BuiltinType::Suspense => todo!(),
             BuiltinType::Teleport => todo!(),
