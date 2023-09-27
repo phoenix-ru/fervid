5c9341331725d8779866c066354e0e598a05610f
erkelost <1256029807@qq.com>
2023-09-27 17:44:25 +0800

    chore: update config

---
+++ crates/fervid_napi/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.spec.ts'],
+    coverage: {
+      reporter: ['json'],
+    },
+    environment: 'node',
+    deps: {
+      interopDefault: false,
+    },
+  },
+})
