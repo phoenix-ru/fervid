4c794419cca59c30a57c9f8e4545a40aa9dcc738
Marsel Shaikhin <phoenix.apps@proton.me>
2024-06-22 23:20:47 +0200

    chore: upgrade deps to resolve security issues

--- crates/fervid_napi/index.js
+++ crates/fervid_napi/index.js
@@ -208,12 +208,62 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, 'napi.linux-arm-gnueabihf.node'))
+        if (isMusl()) {
+          localFileExisted = existsSync(join(__dirname, 'napi.linux-arm-musleabihf.node'))
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./napi.linux-arm-musleabihf.node')
+            } else {
+              nativeBinding = require('@fervid/napi-linux-arm-musleabihf')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        } else {
+          localFileExisted = existsSync(join(__dirname, 'napi.linux-arm-gnueabihf.node'))
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./napi.linux-arm-gnueabihf.node')
+            } else {
+              nativeBinding = require('@fervid/napi-linux-arm-gnueabihf')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        }
+        break
+      case 'riscv64':
+        if (isMusl()) {
+          localFileExisted = existsSync(join(__dirname, 'napi.linux-riscv64-musl.node'))
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./napi.linux-riscv64-musl.node')
+            } else {
+              nativeBinding = require('@fervid/napi-linux-riscv64-musl')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        } else {
+          localFileExisted = existsSync(join(__dirname, 'napi.linux-riscv64-gnu.node'))
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./napi.linux-riscv64-gnu.node')
+            } else {
+              nativeBinding = require('@fervid/napi-linux-riscv64-gnu')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        }
+        break
+      case 's390x':
+        localFileExisted = existsSync(join(__dirname, 'napi.linux-s390x-gnu.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./napi.linux-arm-gnueabihf.node')
+            nativeBinding = require('./napi.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('@fervid/napi-linux-arm-gnueabihf')
+            nativeBinding = require('@fervid/napi-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
